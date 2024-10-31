66a266ef9024982d21b6a57ce7b156a8cead5b30
ghpancardo <ghpancardo@gmail.com>
2024-10-31 03:52:02 -0600

    Limpia

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,19 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    cambiarEstado();
-});
-
 function cambiarEstado() {
-    const botonMenu = document.getElementById("menu-boton");
-    const contenidoMenu = document.getElementById("menu-contenido");
-
-    botonMenu.addEventListener("click", function(event) {
-        event.stopPropagation();
-        contenidoMenu.style.display = contenidoMenu.style.display === "block" ? "none" : "block";
-    });
-
-    document.addEventListener("click", function(event) {
-        if (!botonMenu.contains(event.target)) {
-            contenidoMenu.style.display = "none";
-        }
-    });
-};
+    const dropdown = document.querySelector('.dropdown');
+    dropdown.classList.toggle('is-active');
+  }
+  
+  document.addEventListener('click', (event) => {
+    const dropdown = document.querySelector('.dropdown');
+    const button = document.getElementById('menu-boton');
+    
+    if (!button.contains(event.target) && !dropdown.contains(event.target)) {
+      dropdown.classList.remove('is-active');
+    }
+  });
+  
