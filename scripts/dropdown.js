a74b2782f4e98705f6b52121710323e48b2007d4
ana-rosales <anapaula161103@gmail.com>
2024-11-04 21:06:27 -0600

    Funcion dropdown con clase como parametro

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,5 +1,5 @@
-function cambiarEstado() {
-    const dropdown = document.querySelector('.contenidoDespliegue');
+function cambiarEstado(contenidoDespliegue) {
+    const dropdown = document.querySelector('.'+contenidoDespliegue);
     dropdown.classList.toggle('is-active');
   }
   
