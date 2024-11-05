cdf303b34110cc096f119fadf44eb50a15accbe9
ana-rosales <anapaula161103@gmail.com>
2024-11-04 18:44:07 -0600

    Cambio a estilo css nativo

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,14 +1,13 @@
 function cambiarEstado() {
-    const dropdown = document.querySelector('.dropdown');
-    dropdown.classList.toggle('is-active');
-  }
+    document.getElementById("menuEventos").classList.toggle("mostrar");
+}
   
-  document.addEventListener('click', (event) => {
-    const dropdown = document.querySelector('.dropdown');
-    const button = document.getElementById('menu-boton');
-    
-    if (!button.contains(event.target) && !dropdown.contains(event.target)) {
-      dropdown.classList.remove('is-active');
-    }
-  });
+// document.addEventListener('click', (event) => {
+//   const dropdown = document.querySelector('.dropdown');
+//   const button = document.getElementById('menu-boton');
+  
+//   if (!button.contains(event.target) && !dropdown.contains(event.target)) {
+//     dropdown.classList.remove('is-active');
+//   }
+// });
   
