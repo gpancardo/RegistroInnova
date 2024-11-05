631bb18f4631c3cc5044dabcc435d0123c81b04e
ghpancardo <ghpancardo@gmail.com>
2024-11-04 19:23:01 -0600

    Correcci[on

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,13 +1,14 @@
 function cambiarEstado() {
-    document.getElementById("menuEventos").classList.toggle("mostrar");
-}
+    const dropdown = document.querySelector('.dropdown');
+    dropdown.classList.toggle('is-active');
+  }
   
-// document.addEventListener('click', (event) => {
-//   const dropdown = document.querySelector('.dropdown');
-//   const button = document.getElementById('menu-boton');
-  
-//   if (!button.contains(event.target) && !dropdown.contains(event.target)) {
-//     dropdown.classList.remove('is-active');
-//   }
-// });
+  document.addEventListener('click', (event) => {
+    const dropdown = document.querySelector('.dropdown');
+    const button = document.getElementById('menu-boton');
+    
+    if (!button.contains(event.target) && !dropdown.contains(event.target)) {
+      dropdown.classList.remove('is-active');
+    }
+  });
   
