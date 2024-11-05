68ce1f661132ec2b213c7d003ff1be3bbb10904f
ghpancardo <ghpancardo@gmail.com>
2024-11-04 19:35:43 -0600

    Cambio clase

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,10 +1,10 @@
 function cambiarEstado() {
-    const dropdown = document.querySelector('.dropdown');
+    const dropdown = document.querySelector('.contenidoDespliegue');
     dropdown.classList.toggle('is-active');
   }
   
   document.addEventListener('click', (event) => {
-    const dropdown = document.querySelector('.dropdown');
+    const dropdown = document.querySelector('.contenidoDespliegue');
     const button = document.getElementById('menu-boton');
     
     if (!button.contains(event.target) && !dropdown.contains(event.target)) {
