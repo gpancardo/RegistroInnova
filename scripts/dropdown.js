71a68ab73d889c56c654f5e1ac7223d4ce571729
ana-rosales <anapaula161103@gmail.com>
2024-11-04 21:16:24 -0600

    Cambios al js

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,14 +1,14 @@
-function cambiarEstado(contenidoDespliegue) {
-    const dropdown = document.querySelector('.'+contenidoDespliegue);
+function cambiarEstado() {
+    const dropdown = document.querySelector('.contenidoDespliegue');
     dropdown.classList.toggle('is-active');
   }
   
-  document.addEventListener('click', (event) => {
-    const dropdown = document.querySelector('.contenidoDespliegue');
-    const button = document.getElementById('menu-boton');
-    
-    if (!button.contains(event.target) && !dropdown.contains(event.target)) {
-      dropdown.classList.remove('is-active');
-    }
-  });
+document.addEventListener('click', (event) => {
+  const dropdown = document.querySelector('.contenidoDespliegue');
+  const button = document.getElementById('menu-boton');
+  
+  if (!button.contains(event.target) && !dropdown.contains(event.target)) {
+    dropdown.classList.remove('is-active');
+  }
+});
   
