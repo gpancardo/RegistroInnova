3f27917b07aeb8838e2a0b368b4bd54f2f3625a4
ana-rosales <anapaula161103@gmail.com>
2024-11-04 23:42:23 -0600

    Creacion del enrutador

---
+++ scripts/router.js
@@ -0,0 +1,20 @@
+let index = document.querySelector('#index');
+let inicioSesion = document.querySelector('#inicioSesion');
+let registro = document.querySelector('#registro');
+let panelAdmon = document.querySelector('#panelAdmon');
+
+index.addEventListener('click',()=>{
+    window.location.href = 'index.html'
+});
+
+// inicioSesion.addEventListener('click',()=>{
+//     window.location.href = 'inicioSesion.html'
+// });
+
+// registro.addEventListener('click',()=>{
+//     window.location.href = 'registro.html'
+// });
+
+// panelAdmon.addEventListener('click',()=>{
+//     window.location.href = 'panelAdmon.html'
+// });
